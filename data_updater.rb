e1d89492845898895f691ee45d3cf3ed67875ba7
PoweredByRobots <robots@bitsurf.org>
2019-02-12 15:34:38 -0800

    extract song-related stuff to its own class

--- data_updater.rb
+++ data_updater.rb
@@ -2,11 +2,8 @@
 
 require_relative 'lib/mysql'
 require_relative 'lib/filehandler'
-require 'nokogiri'
-require 'open-uri'
+require_relative 'lib/song'
 require 'pry'
-
-include Mysql
 
 def filename
   'processed.songs'
@@ -16,88 +13,31 @@ def file
   @file ||= FileHandler.new(filename)
 end
 
+def database
+  @database ||= Mysql.new
+end
+
 def remove_already_processed(songs)
   already_processed = file.processed_ids
   songs.select { |song| !already_processed.include? song.first }
 end
 
-def update_song(id, artist, title)
-  puts "#{artist} - #{title}"
-  update_data(id, artist, title)
-  file.add_to_list(id)
-end
-
-def xpath(doc, name)
-  doc.xpath(paths[name.to_sym]).children.to_s
-rescue => error
-  puts error.message
-end
-
-def webdoc(path)
-  base_url = 'https://tunebat.com'
-  Nokogiri::HTML(open(base_url + path))
-rescue => error
-  puts error.message
-end
-
-def lookup_data(artist, title)
-  path = '/Search?q=' + (artist + ' ' + title).tr(' ', '+')
-  doc = webdoc(sanitize(path))
-  path_to_link = '/html/body/div[1]/div[1]/div/' \
-                 'div/div[2]/div[2]/div[1]/div/a'
-  link_data = doc.xpath(path_to_link).first
-  return unless link_data
-  link = link_data.attributes['href'].value
-  extract_data_from_paths(webdoc(link))
-rescue => error
-  puts error.message
-end
-
-def extract_data_from_paths(doc)
-  data = {}
-  paths.keys.each { |a| data[a] = xpath(doc, a) }
-  return if data[:happiness].nil?
-  data
-end
-
-def paths
-  { happiness: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[3]',
-    danceability: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[2]',
-    energy: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[1]',
-    accousticness: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[5]', 
-    instrumentalness: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[6]', 
-    liveness: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[7]',
-    speechiness: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[3]/div/div[1]/table/tbody/tr[2]/td[8]',
-    album: '/html/body/div[1]/div[2]/div/div/div[1]/div/div/div/div[2]/table/tbody/tr[2]/td[2]' }
-end
-
-def update_data(id, artist, title)
-  @backlog -= 1
-  pause
-  data = lookup_data(artist, title)
+def update_song(song)
+  puts "#{song.artist} - #{song.title}"
+  data = song.attributes
   return unless data
-  data[:album] = escape_apostrophes(data[:album])
-  attributes = ''
-  paths.keys.each { |a| attributes += "#{a} = '#{data[a.to_sym]}', " }
-  puts data.to_s
-  update_db(id, attributes[0..-3])
-end
-
-def escape_apostrophes(string)
-  return unless string
-  string.gsub("'", "\\\\'")
-end
-
-def sanitize(string)
-  string.gsub(/[\u0080-\u00ff]/, '')
-end
-
-def pause
-  sleep 10
+  database.update(song.id, data)
+  file.add_to_list(song.id)
 end
 
 system 'clear'
-songs = remove_already_processed(songlist('AND happiness = 0'))
-@backlog = songs.count
-puts "-==[#{@backlog}]==- songs to go!"
-songs.each { |id, artist, title| update_song(id, artist, title) }
+
+songs = remove_already_processed(database.songs('AND happiness = 0'))
+backlog = songs.count
+puts "-==[#{backlog}]==- songs to go!"
+
+songs.each do |id, artist, title|
+  song = Song.new(id, artist, title)
+  update_song(song)
+  backlog -= 1
+end
