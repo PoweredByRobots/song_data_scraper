1a002bb4980308c78f02fb8f4d86a29148068abf
PoweredByRobots <robots@bitsurf.org>
2019-02-16 22:21:45 -0800

    add acceptable genre list

---
+++ lib/acceptable_genres.rb
@@ -0,0 +1,46 @@
+def acceptable_genres
+  %w(
+    african
+    britpop
+    canadian
+    classic\ rock
+    classical
+    comedy
+    country
+    dance
+    dance-rock
+    edm
+    electro-swing
+    french
+    grunge
+    heavy\ metal
+    hip\ hop
+    house
+    indie\ rock
+    jazz
+    latin
+    mexican
+    motown
+    oldies
+    pbr\ n\ b
+    pop
+    psychedelic
+    punk
+    r&b
+    reggae
+    rock
+    shoegaze
+    ska
+    soul
+    space\ rock
+    summer
+    surf
+    synthpop
+    top40
+    trance
+    trap\ music
+    trip\ hop
+    weird
+    world
+    yacht\ rock)
+end
