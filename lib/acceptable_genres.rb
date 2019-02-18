6f514d27aa6c6816b3d2b4375cee7a168d4f3f3e
PoweredByRobots <robots@bitsurf.org>
2019-02-18 01:42:42 -0800

    Update acceptable_genres.rb

--- lib/acceptable_genres.rb
+++ lib/acceptable_genres.rb
@@ -1,46 +1,132 @@
 def acceptable_genres
-  {
+  { 'acoustic': %w(accoustic),
     'african': %w(afrobat),
-    'britpop': %w(brit\ pop british\ indie\ rock british\ indie\ rock\ pop british\ indie\ rock/pop),
-    'canadian': %w(vancouver\ indie canadian\ country canadian\ indie canadian\ metal canadian\ pop),
+    'ambient': [],
+    'art01': [],
+    'britpop': %w(brit\ pop
+                  british\ indie\ rock
+                  british\ indie\ rock\ pop
+                  british\ indie\ rock/pop),
+    'canadian': %w(vancouver\ indie
+                   canadian\ country
+                   canadian\ indie
+                   canadian\ metal
+                   canadian\ pop),
+    'cday18': %w(),
+    'christmas': [],
     'classic rock': %w(70s),
-    'classical': %w(), 
+    'classical': [], 
     'comedy': %w(humour funny comedy\ rock comedy/spoken comic),
-    'country': %w(cowboy\ western country-rock country\ gospel country\ rock),
-    'dance': %w(euro\ house euro-dance electronica/dance electronica\ dance electronica\ and\ dance electro electro house electroclash edm house disco\ house disco club club/dance dance\ pop dance/electronic),
+    'country': %w(cowboy\ western
+                  country-rock
+                  country\ gospel
+                  country\ rock),
+    'dance': %w(euro\ house
+                euro-dance
+                electronica/dance
+                electronica\ dance
+                electronica\ and\ dance
+                electro
+                electro\ house
+                electroclash
+                edm
+                house
+                disco\ house
+                disco
+                club
+                club/dance
+                dance\ pop
+                dance/electronic),
     'dance-rock': %w(dance\ rock),
+    'dhr': [],
     'electro-swing': %w(electro\ swing electroswing),
-    'french': %w(french\ rock french\ reggae french\ indietronica french\ pop france french\ indie\ pop),
+    'folk': %w(traditional\ folk
+               alt\ folk
+               alt.\ folk
+               alternative\ folk
+               british\ folk
+               british\ folk-rock
+               contemporary\ folk
+               folk\ &\ bluegrass
+               folk\ christmas
+               folk\ rock
+               folk-pop
+               folk-rock
+               folk/rock),
+    'fraser18': [],
+    'french': %w(french\ rock
+                 french\ reggae
+                 french\ indietronica
+                 french\ pop
+                 france
+                 french\ indie\ pop),
     'grunge': %w(grunge\ pop),
-    'metal': %w(hair\ metal),
-    'hip hop': %w(hip-hop hiphop hip\ hop/rap hip\ hop\ rap gangster\ rap east\ coast\ rap rap east\ coast\ hip\ hop conscious\ hip\ hop abstract\ hip\ hop breakbeat breaks),
-    'indie rock': %w(brooklyn\ indie alternative alternative\ rock alternative/indie alternative/indie\ rock alternrock art\ rock),
-    'indie dance': %w(),
-    'jazz': %w(),
-    'latin': %w(),
-    'mexican': %w(),
-    'motown': %w(),
-    'oldies': %w(),
-    'pbr&b': %w(),
+    'metal': %w(hair\ metal heavy\ metal),
+    'hip hop': %w(hip-hop
+                  hiphop
+                  hip\ hop/rap
+                  hip\ hop\ rap
+                  gangster\ rap
+                  east\ coast\ rap
+                  rap east\ coast\ hip\ hop
+                  conscious\ hip\ hop
+                  abstract\ hip\ hop
+                  breakbeat
+                  breaks),
+    'indie rock': %w(deep\ australian\ indie
+                     indie
+                     indie\ dream\ pop
+                     indie\ folk
+                     indie\ fuzzpop
+                     indie\ garage\ rock
+                     indie\ pop
+                     indie\ pop/synth-pop
+                     indie\ poptimism
+                     indie\ psych-pop
+                     indie\ psych-rock
+                     indie\ rockism
+                     brooklyn\ indie
+                     alternative
+                     alternative\ rock
+                     alternative/indie
+                     alternative/indie\ rock
+                     alternrock
+                     art\ rock),
+    'indie dance': [],
+    'jazz': %w(jazz\ trip-hop jazz\ funk jazz\ christmas japanese\ jazztronica indie\ jazz smooth\ jazz soul-jazz vocal\ jazz acid\ jazz dark\ jazz electro\ jazz finnish\ jazz),
+    'latin': [],
+    'mexican': [],
+    'motown': [],
+    'oldies': [],
+    'pbr&b': %w(pbr\ n\ b pbr\ n'\ b),
     'pop': %w(acoustic\ pop adult\ alternative\ pop/rock adult\ alternative\ pop),
-    'psychedelic': %w(),
-    'punk': %w(emo\ punk alternative\ punk),
+    'prog rock': %w(progressive\ rock),
+    'psychedelic': %w(indie\ psych-rock),
+    'punk': %w(emo\ punk alternative\ punk indie\ punkß),
     'r&b': %w(rnb r\ and\ b r\ b\ r\ b\ hip\ hop\ r\ b\ soul r&b r&b-soul),
     'reggae': %w(reggaeton),
-    'rock': %w(experimental-rock experimental\ rock college\ rock blues\ rock alt\ pop\ rock alt\ rock album\ rock acid\ rock adult\ alternative\ rock),
+    'rock': %w(experimental-rock
+               experimental\ rock
+               college\ rock
+               blues\ rock
+               alt\ pop\ rock
+               alt\ rock
+               album\ rock
+               acid\ rock
+               adult\ alternative\ rock),
     'shoegaze': %w(indie\ shoegaze),
-    'ska': %w(),
-    'soul': %w(),
-    'space rock': %w(),
-    'summer': %w(),
-    'surf': %w(),
-    'synthpop': %w(),
+    'ska': %w(ska\ revival traditional\ ska),
+    'soul': [],
+    'space rock': [],
+    'summer': [],
+    'surf': [],
+    'synthpop': [],
     'top40': %w(top\ 40),
-    'trance': %w(),
-    'trap music': %w(),
+    'trance': [],
+    'trap music': [],
     'trip hop': %w(trip-hop triphop),
-    'weird': %w(),
+    'weird': [],
     'world': %w(world\ chill),
-    'yacht rock': %w()
+    'yacht rock': []
   }
 end
